fc577224a5d69f8581d17cb75a26270eab3db6f4
LiseRochat <liserochat@live.fr>
2022-04-15 10:56:37 +0200

    Clean Connection Controller

--- Controller/InitConnectionController.php
+++ Controller/InitConnectionController.php
@@ -7,20 +7,11 @@ require("../Model/BDDClass.php");
 require("../Model/StorageClass.php");
 require("../Controller/BDDController.php");
 
-/*************************************** Inscription Utilisateur *************************************** */
-$bdd->newUser($bddConnection,
-                $_POST['firstnameIns'],
-                $_POST['lastnameIns'],
-                $_POST['emailIns'],
-                $_POST['passwordIns'],
-                '1'
-            );
-
 /*************************************** Connexion Utilisateur *************************************** */
 // On recupère les données du formulaire de connexion avec la methode POST et on fait apelle a la méthode login de notre class BDD
 $user = $bdd->login($bddConnection, $_POST['password'], $_POST['email']);
 
-/*************************************** Sauveagrde S_SESSION*************************************** */
+/*************************************** Sauvegarde S_SESSION*************************************** */
 // On instancie un nouvel objet Storage
 $sessionUserSignIng = new Storage();
 // On sauvegarde ses données dans la variable globale $_SESSIN a l'aide de la methode de notre classe on récupère un tableau de donnée
