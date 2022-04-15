1cd4f776ea492fc446d78e141f49afd3ed311089
LiseRochat <liserochat@live.fr>
2022-04-15 09:45:49 +0200

    Add comment on BDD class

--- Model/BDDClass.php
+++ Model/BDDClass.php
@@ -1,22 +1,25 @@
+<!-- Classe pour effectuer toutes mes requetes SQL dont la connexion à la BDD -->
 <?php
-
 class BDD {
 
     CONST user = 'root';
     CONST password = '';
 
+    // Connexion à la base de donnée
     public function connection() {
 
+        // Retourne l'objet bdd en cas de succès
         try {
             $bdd = new PDO("mysql:host=localhost;dbname=connection", self::user, self::password);
             return $bdd;
-
+        // Retourne le message d'erreur et arrête le script en cas d'échec
         } catch (PDOException $e) {
             print "Erreur !: " . $e->getMessage() . "<br/>";
             die();
         }
     }
 
+    // Methode permettant la création d'un nouvel utlisateur
     public function newUser($bdd,$name, $username,$email,$password,$role) {
 
         $passwordHash = $this->hashPassword($password);
@@ -26,25 +29,23 @@ class BDD {
 
     }
 
+    // Methode permettant de hasher le mot de passe avant enregistrement en bdd
     public function hashPassword($password) {
 
         return password_hash($password, PASSWORD_DEFAULT);
     }
 
+    // Methode permettant la connexion d'un utilisateur existant
     public function login($bdd, $password, $email) {
 
         $sql = "SELECT * FROM user WHERE email = '$email'";
         $query = $bdd->query($sql)->fetch();
 
         if(password_verify($password, $query['password'])) {
-            
             echo "connecter !!";
             return $query;
-
         } else {
-            
             echo "try again !!";
         }
-
     }
 }
