35698bcc9734e36d0cb5c96ed8a99adc2f1b150b
LiseRochat <liserochat@live.fr>
2022-04-15 16:21:04 +0200

    Add Patern on input

--- Vue/inscription.php
+++ Vue/inscription.php
@@ -12,13 +12,13 @@
         <h1>Inscription</h1>
         <form method="post" action="../Controller/InitInscriptionController.php">
             <label for="firstname">Prenom</label>
-            <input type="text" name="firstname" placeholder="votre prénom" id="firstname" required maxlength=30>
+            <input type="text" name="firstname" placeholder="votre prénom" id="firstname" required maxlength=30 pattern="^[A-Za-z '-]+$">
             <label for="lastname">Nom</label>
-            <input type="text" name="lastname" placeholder="votre nom" id="lastname" required maxlength=30>
+            <input type="text" name="lastname" placeholder="votre nom" id="lastname" required maxlength=30 pattern="^[A-Za-z '-]+$">
             <label for="email">Email</label>
-            <input type="text" name="email" placeholder="email" id="email" required maxlength=254>
+            <input type="text" name="email" placeholder="email" id="email" required maxlength=254 pattern="^[A-Za-z]+@{1}[A-Za-z]+\.{1}[A-Za-z]{2,}$">
             <label for="password">Mot de Passe</label>
-            <input type="password" name="password" placeholder="password" id="password" required minlenght=6 maxlength=255>
+            <input type="password" name="password" placeholder="password" id="password" required minlenght=6 maxlength=255 pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{6,}$">
             <input type="submit" value="S'Inscrire">
         </form>
     </div>
