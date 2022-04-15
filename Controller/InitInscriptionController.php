92cd91c433816887e3f5cb7bbf7e18031679854e
LiseRochat <liserochat@live.fr>
2022-04-15 16:09:04 +0200

    Make Browser side security

--- Controller/InitInscriptionController.php
+++ Controller/InitInscriptionController.php
@@ -7,13 +7,14 @@ require("../Controller/BDDController.php");
 /*************************************** Inscription Utilisateur *************************************** */
 
 if(!empty($_POST)) {
+    // On verifie que tous les champs du formulaire sont remplis
     if(!empty($_POST['firstname'] && !empty($_POST['lastname']) && !empty($_POST['email']) && !empty($_POST['password']))) {
-            $bdd->newUser($bddConnection,
-            $_POST['firstname'],
-            $_POST['lastname'],
-            $_POST['email'],
-            $_POST['password'],
-            '1'
-        );
+        $bdd->newUser($bddConnection,
+                $_POST['firstname'],
+                $_POST['lastname'],
+                $_POST['email'],
+                $_POST['password'],
+                '1'
+            );
     }
 }
--- Vue/inscription.php
+++ Vue/inscription.php
@@ -12,13 +12,13 @@
         <h1>Inscription</h1>
         <form method="post" action="../Controller/InitInscriptionController.php">
             <label for="firstname">Prenom</label>
-            <input type="text" name="firstname" placeholder="votre prénom" firstname" required>
+            <input type="text" name="firstname" placeholder="votre prénom" id="firstname" required maxlength=30>
             <label for="lastname">Nom</label>
-            <input type="text" name="lastname" placeholder="votre nom" id="lastname" required>
+            <input type="text" name="lastname" placeholder="votre nom" id="lastname" required maxlength=30>
             <label for="email">Email</label>
-            <input type="text" name="email" placeholder="email" id="email" required>
+            <input type="text" name="email" placeholder="email" id="email" required maxlength=254>
             <label for="password">Mot de Passe</label>
-            <input type="password" name="password" placeholder="password" id="password" required>
+            <input type="password" name="password" placeholder="password" id="password" required minlenght=6 maxlength=255>
             <input type="submit" value="S'Inscrire">
         </form>
     </div>
