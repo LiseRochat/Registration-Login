5c47406b8ebd06adc14f788678768224d043fdcd
LiseRochat <liserochat@live.fr>
2022-04-15 10:24:41 +0200

    Create Inscription Form

---
+++ Vue/inscription.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fomrulaire d'Inscription | PHP POO</title>
+    <meta name="description" content="Formulaire d'inscritpion en PHP POO sous le modèle MVC. Sauvegarde en Base de donnée.">
+</head>
+<body>
+    <div class="card">
+        <h1>Inscription</h1>
+        <form method="post" action="InitConnectionController.php">
+            <label for="firstname">Prenom</label>
+            <input type="text" name="firstname" placeholder="votre prénom" firstname" required>
+            <label for="lastname">Nom</label>
+            <input type="text" name="lastname" placeholder="votre nom" id="lastname" required>
+            <label for="email">Email</label>
+            <input type="text" name="email" placeholder="email" id="email" required>
+            <label for="password">Mot de Passe</label>
+            <input type="password" name="password" placeholder="password" id="password" required>
+            <input type="submit" value="S'Inscrire">
+        </form>
+    </div>
+</body>
+</html>
