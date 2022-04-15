e2017da5b200dc1c22761be740855adf2113e30f
LiseRochat <liserochat@live.fr>
2022-04-15 10:15:41 +0200

    Add comments on InitConnectionController.php

--- Controller/InitConnectionController.php
+++ Controller/InitConnectionController.php
@@ -1,20 +1,28 @@
 <?php
+// Pour pouvoir utiliser la variable globale de php $_SESSION
 session_start();
 
+// Apelle de nos class
 require("Model/BDDClass.php");
 require("Model/StorageClass.php");
 
+/*************************************** Connexion BDD *************************************** */
+// On crée une nouvelle instance de BDD
+$bdd = new BDD();
+// On utilise la methode connection de notre class BDD pour pouvoir se connecter
+$bddConnection = $bdd->connection();
 
-$connection = new BDD();
+/*************************************** Inscription Utilisateur *************************************** */
+//$bdd->newUser($bddConnection,'lise','rochat','liserochat@live.fr','azerty','1');
 
-$result = $connection->connection();
+/*************************************** Connexion Utilisateur *************************************** */
+// On recupère les données du formulaire de connexion aec la methode POST et on fait apelle a la méthode login denotre class BDD
+$user = $bdd->login($bddConnection, $_POST['password'], $_POST['email']);
 
-//$connection->newUser($result,'lise','rochat','liserochat@live.fr','azerty','1');
-
-$user = $connection->login($result, $_POST['password'], $_POST['email']);
-
-$session = new Storage();
-
-$session->storeUSerData($user);
-
-var_dump($session->getUserData()['firstname']);
+/*************************************** Sauveagrde S_SESSION*************************************** */
+// On instancie un nouvel objet Storage
+$sessionUserSignIng = new Storage();
+// On sauvegarde ses données dans la variable globale $_SESSIN a l'aide de la methode de notre classe on récupère un tableau de donnée
+$sessionUserSignIng->storeUSerData($user);
+// On affiche ses données soit toutes soit une valeur en particulier
+var_dump($sessionUserSignIng->getUserData()['firstname']);
