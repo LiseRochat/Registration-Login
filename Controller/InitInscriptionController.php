58ec7ada3ee8a7ec042bb383ce1211a4542b31b8
LiseRochat <liserochat@live.fr>
2022-04-15 16:45:10 +0200

    Use validationData method on Controller

--- Controller/InitInscriptionController.php
+++ Controller/InitInscriptionController.php
@@ -9,12 +9,11 @@ require("../Controller/BDDController.php");
 if(!empty($_POST)) {
     // On verifie que tous les champs du formulaire sont remplis
     if(!empty($_POST['firstname'] && !empty($_POST['lastname']) && !empty($_POST['email']) && !empty($_POST['password']))) {
-        $bdd->newUser($bddConnection,
-                $_POST['firstname'],
-                $_POST['lastname'],
-                $_POST['email'],
-                $_POST['password'],
-                '1'
-            );
+        // On nettoie les donnés recu avec la methode validationData de notre classe BDD
+        $firstname = $bdd->validationData($_POST['firstname']);
+        $lastname = $bdd->validationData($_POST['lastname']);
+        $email = $bdd->validationData($_POST['email']);
+        $password = $bdd->validationData($_POST['password']);
+        $bdd->newUser($bddConnection, $fristname, $lastname, $email, $password, 1);
     }
 }
