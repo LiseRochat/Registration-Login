be6fb49e5febbb481d82b109a7bdf8044160e63c
LiseRochat <liserochat@live.fr>
2022-04-23 11:04:55 +0200

    Displaying data in the view

--- Views/Visitors/home.php
+++ Views/Visitors/home.php
@@ -1,17 +1,9 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fomrulaire d'Inscription | PHP POO</title>
-    <meta name="description" content="Formulaire d'inscritpion en PHP POO sous le modèle MVC. Sauvegarde en Base de donnée.">
-    <link rel="stylesheet" href="./style/style.css">
-</head>
-<body>
-  
-    <h1>Bonjour</h1>
+<h1>Bonjour</h1>
+<?php 
+    foreach ($users as $user) {
+        echo "Bonjour " . $user['firstname']. " " . $user["lastname"];
+    }
+?>
+
        
  
-</body>
-</html>
