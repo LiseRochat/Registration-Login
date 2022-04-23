e9bd4e381ab6e1109b64de590aab9249205dc6a9
LiseRochat <liserochat@live.fr>
2022-04-23 10:47:30 +0200

    SQL requetes

--- Models/MainManager.php
+++ Models/MainManager.php
@@ -4,11 +4,5 @@ require_once("Model.class.php");
 
 abstract class MainManager extends Model{
 
-    public function getDatas(){
-        $req = $this->getBdd()->prepare("SELECT * FROM matable");
-        $req->execute();
-        $datas = $req->fetchAll(PDO::FETCH_ASSOC);
-        $req->closeCursor();
-        return $datas;
-    }
+   
 }
--- Models/Visitors/VisitorsModel.php
+++ Models/Visitors/VisitorsModel.php
@@ -1,4 +1,11 @@
 <?php
+require_once("./Models/MainManager.php");
 class VisitorsManager extends MainManager {
-    
+    public function getDatas(){
+        $req = $this->getBdd()->prepare("SELECT * FROM user");
+        $req->execute();
+        $datas = $req->fetchAll(PDO::FETCH_ASSOC);
+        $req->closeCursor();
+        return $datas;
+    }
 }
