b93bf86dcacea7646a0be75d6cded872b66f3e4d
LiseRochat <liserochat@live.fr>
2022-04-23 10:27:08 +0200

    Add one user in database

--- user.sql
+++ user.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : sam. 23 avr. 2022 à 08:21
+-- Généré le : sam. 23 avr. 2022 à 08:26
 -- Version du serveur : 5.7.36
 -- Version de PHP : 7.3.33
 
@@ -37,9 +37,16 @@ CREATE TABLE IF NOT EXISTS `user` (
   `role` varchar(50) NOT NULL,
   `avatar` varchar(100) NOT NULL,
   `isValid` tinyint(4) NOT NULL,
-  `keyValidation` int(11) NOT NULL,
+  `keyValidation` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_user`)
-) ENGINE=MyISAM AUTO_INCREMENT=29 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=30 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id_user`, `firstname`, `lastname`, `email`, `password`, `role`, `avatar`, `isValid`, `keyValidation`) VALUES
+(29, 'test', 'test', 'test@gmail.com', '$2y$10$pWXetC6v7FHpIYjKMQpB7.2vcY.hkb56Js./sizqwaVYOLuC2.9gi', 'administrator', '', 1, NULL);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
