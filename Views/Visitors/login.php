1b466d9f8060f1c64cd72d29c7eeefda89b0a819
LiseRochat <liserochat@live.fr>
2022-04-23 11:16:57 +0200

    Update view login.php

---
+++ Views/Visitors/login.php
@@ -0,0 +1,10 @@
+<div class="card">
+    <h1>Connexion</h1>
+    <form method="post" action="../Controller/InitConnectionController.php">
+        <label for="email">Email</label>
+        <input type="text" name="email" placeholder="email" id="email" required>
+        <label for="password">Mot de Passe</label>
+        <input type="password" name="password" placeholder="password" id="password" required>
+        <input class="submit" type="submit" value="Connexion">
+    </form>
+</div> 
