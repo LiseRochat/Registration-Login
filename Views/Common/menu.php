e177d0eb181869de0bb030480cad6282328e103d
LiseRochat <liserochat@live.fr>
2022-04-27 12:37:08 +0200

    Add new view profil

--- Views/Common/menu.php
+++ Views/Common/menu.php
@@ -1,6 +1,10 @@
 <nav>
     <ul>
         <li><a href="<?php echo URL; ?>accueil">Accueil</a></li>
+        <?php if (empty($_SESSION['profil'])) :?>
         <li><a href="<?php echo URL; ?>login">Se connecter</a></li>
+        <?php else : ?>
+        <li><a href="<?php echo URL; ?>compt/profil">Mon Profil</a></li>
+        <?php endif; ?>
     </ul>
 </nav>
