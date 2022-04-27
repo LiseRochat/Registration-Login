47f53955d16e4dc6fa13ac334806d8b8493e8621
LiseRochat <liserochat@live.fr>
2022-04-27 13:45:54 +0200

    Add link "Creer Compte" in menu.php

--- Views/Common/menu.php
+++ Views/Common/menu.php
@@ -3,6 +3,7 @@
         <li><a href="<?php echo URL; ?>accueil">Accueil</a></li>
         <?php if (empty($_SESSION['profil'])) :?>
         <li><a href="<?php echo URL; ?>login">Se connecter</a></li>
+        <li><a href="<?php echo URL; ?>creerCompte">Créer un compte</a></li>
         <?php else : ?>
         <li><a href="<?php echo URL; ?>compte/profil">Mon Profil</a></li>
         <li><a href="<?php echo URL; ?>compte/deconnexion">Se déconnecter</a></li>
--- Views/inscription.php
+++ Views/inscription.php
@@ -1,27 +1,14 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fomrulaire d'Inscription | PHP POO</title>
-    <meta name="description" content="Formulaire d'inscritpion en PHP POO sous le modèle MVC. Sauvegarde en Base de donnée.">
-    <link rel="stylesheet" href="./style/style.css">
-</head>
-<body>
-    <div class="card">
-        <h1>Inscription</h1>
-        <form method="post" action="../Controller/InitInscriptionController.php">
-            <label for="firstname">Prenom</label>
-            <input type="text" name="firstname" placeholder="votre prénom" id="firstname" required maxlength=30 pattern="^[A-Za-z '-]+$">
-            <label for="lastname">Nom</label>
-            <input type="text" name="lastname" placeholder="votre nom" id="lastname" required maxlength=30 pattern="^[A-Za-z '-]+$">
-            <label for="email">Email</label>
-            <input type="text" name="email" placeholder="email" id="email" required maxlength=254 pattern="^[A-Za-z]+@{1}[A-Za-z]+\.{1}[A-Za-z]{2,}$">
-            <label for="password">Mot de Passe</label>
-            <input type="password" name="password" placeholder="password" id="password" required minlenght=6 maxlength=255>
-            <input class="submit" type="submit" value="S'Inscrire">
-        </form>
-    </div>
-</body>
-</html>
+<div class="card">
+    <h1>Inscription</h1>
+    <form method="post" action="../Controller/InitInscriptionController.php">
+        <label for="firstname">Prenom</label>
+        <input type="text" name="firstname" placeholder="votre prénom" id="firstname" required maxlength=30 pattern="^[A-Za-z '-]+$">
+        <label for="lastname">Nom</label>
+        <input type="text" name="lastname" placeholder="votre nom" id="lastname" required maxlength=30 pattern="^[A-Za-z '-]+$">
+        <label for="email">Email</label>
+        <input type="text" name="email" placeholder="email" id="email" required maxlength=254 pattern="^[A-Za-z]+@{1}[A-Za-z]+\.{1}[A-Za-z]{2,}$">
+        <label for="password">Mot de Passe</label>
+        <input type="password" name="password" placeholder="password" id="password" required minlenght=6 maxlength=255>
+        <input class="submit" type="submit" value="S'Inscrire">
+    </form>
+</div>
