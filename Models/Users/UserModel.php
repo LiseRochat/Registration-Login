bf330261fbe42f9d7cf6c8fc48055bb341f59add
LiseRochat <liserochat@live.fr>
2022-05-02 10:36:06 +0200

    Update sql requete

--- Models/Users/UserModel.php
+++ Models/Users/UserModel.php
@@ -45,14 +45,15 @@ class UserManager extends MainManager {
         return empty($user);
     }
 
-    public function dbCreationAccount($firstname, $lastname, $email, $passwordCrypte, $key) {
+    public function dbCreationAccount($firstname, $lastname, $email, $passwordCrypte, $key, $image) {
         $req = "INSERT INTO user (firstname, lastname, email, password, role, avatar, isValid, keyValidation)
-                VALUES (:firstname, :lastname, :email, :password, 'utilisateur', '', 0, :key )";
+                VALUES (:firstname, :lastname, :email, :password, 'utilisateur', :avatar, 0, :key )";
         $stmt = $this->getBDD()->prepare($req);
         $stmt->bindValue(":firstname", $firstname, PDO::PARAM_STR);
         $stmt->bindValue(":lastname", $lastname, PDO::PARAM_STR);
         $stmt->bindValue(":email", $email, PDO::PARAM_STR);
         $stmt->bindValue(":password", $passwordCrypte, PDO::PARAM_STR);
+        $stmt->bindValue(":avatar", $image, PDO::PARAM_STR);
         $stmt->bindValue(":key", $key, PDO::PARAM_INT);
         $stmt->execute();
         // On conserve le resultat de la requete : si les données sont enregistré isAdd = true sinon false
