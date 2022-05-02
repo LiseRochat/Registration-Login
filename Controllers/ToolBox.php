7481a274d77db02f006ad1a62aea3c9bf1847075
LiseRochat <liserochat@live.fr>
2022-05-02 11:31:21 +0200

    Add avatar on file

--- Controllers/ToolBox.php
+++ Controllers/ToolBox.php
@@ -14,4 +14,29 @@ class ToolBox {
             ToolBox::addMessageAlert("Mail non envoyé");
         }
     }
+
+    public static function addPicture($file, $rep){
+        if(!isset($file['name']) || empty($file['name']))
+            throw new Exception("Vous devez indiquer une image");
+    
+        if(!file_exists($rep)) mkdir($rep,0777);
+    
+        $extension = strtolower(pathinfo($file['name'],PATHINFO_EXTENSION));
+        $random = rand(0,99999);
+        $target_file = $rep.$random."_".$file['name'];
+        
+        if(!getimagesize($file["tmp_name"]))
+            throw new Exception("Le fichier n'est pas une image");
+        if($extension !== "jpg" && $extension !== "jpeg" && $extension !== "png" && $extension !== "gif" && $extension !== "webp")
+            throw new Exception("L'extension du fichier n'est pas reconnu");
+        if(file_exists($target_file))
+            throw new Exception("Le fichier existe déjà");
+        if($file['size'] > 500000)
+            throw new Exception("Le fichier est trop gros");
+        if(!move_uploaded_file($file['tmp_name'], $target_file)) {
+            throw new Exception("l'ajout de l'image n'a pas fonctionné");
+        } else {
+            return ($random."_".$file['name']);
+        }
+    }
 }
--- Controllers/Users/UsersController.php
+++ Controllers/Users/UsersController.php
@@ -59,7 +59,7 @@ class UsersController extends MainController
         if ($this->UserManager->verifEmailAvailable($email)) {
             $passwordCrypte = password_hash($password, PASSWORD_DEFAULT);
             $key = rand(0, 9999);
-            if ($this->UserManager->dbCreationAccount($firstname, $lastname, $email, $passwordCrypte, $key, "/profil.png")) {
+            if ($this->UserManager->dbCreationAccount($firstname, $lastname, $email, $passwordCrypte, $key, "/profils/profil.png")) {
                 $this->sendMailValidation($firstname, $email, $key);
                 ToolBox::addMessageAlert("Le compte à été créé, un mail de validation vous a été envoyé.");
                 header("Location:" . URL . "login");
@@ -142,6 +142,11 @@ class UsersController extends MainController
             ToolBox::addMessageAlert("Les mots de passes ne correspondent pas !");
             header("Location:" . URL . "compte/modificationMotDePasse");
         }
+    }
+
+    public function validationEditAvatar($avatar) {
+        $repertoire = "public/assets/img/profils/".$_SESSION['profil']["email"]."/";
+        ToolBox::addPicture($avatar,$repertoire);
     }
 
     public function deleteAccount()
