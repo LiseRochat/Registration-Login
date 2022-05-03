fbca42f08ebf0be027f117ac5d3f0bd91895abfa
LiseRochat <liserochat@live.fr>
2022-05-03 12:16:05 +0200

    Form connection and inscription

--- Views/Visitors/home.php
+++ Views/Visitors/home.php
@@ -1,6 +1,9 @@
 <h1>Gestion Connexion et Inscription</h1>
+<div class="home">
 <p>Réalisation d'une gestion de compte. Avec une partie administrateur. L'administrateur à la possibilité de gérer les rôles des utilisateurs.</p>
 <p>Le projet est réalisé avec les languages PHP, SQL et JavaScript et les technologies HTML et CSS. La méthode de développement rspecte l'architecture MVC.</p>
+</div>
+
 
 
        
